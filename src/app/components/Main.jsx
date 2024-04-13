0c1336b7bed5204706b234115e0f3ea5fd847d63
Raj Prajapati <118254313+prajapatiraj8212@users.noreply.github.com>
2024-04-13 07:58:24 +0530

    Update Main.jsx

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -8,12 +8,12 @@ const Main = () => {
     <div id='home' className='w-full h-screen text-center'>
          <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-            <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHING TOGETHER</p>
-            <h1 className='py-4 text-gray-700'>Hi, I'm <span className='text-[#5651e5]'>Raj</span></h1>
+            <p className='uppercase text-sm tracking-widest text-gray-600'>LET&aposS BUILD SOMETHING TOGETHER</p>
+            <h1 className='py-4 text-gray-700'>Hi, I&aposm <span className='text-[#5651e5]'>Raj</span></h1>
             <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
             <p className='py-4 text-gray-700 max-w-[70%] m-auto'>
-            I'm a front-end web developer specializing in building (and occasionally designing) exceptional digital
-            experiences. Currently, I'm focused on building responsive front-end web applications while 
+            I&aposm a front-end web developer specializing in building (and occasionally designing) exceptional digital
+            experiences. Currently, I&aposm focused on building responsive front-end web applications while 
             learning back-end technologies.
             </p>
             <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
